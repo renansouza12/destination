1816213020b27e53f2140e2b49b89f68258fe60b
renan <renansouza0122@gmail.com>
2025-03-12 00:58:31 -0400

    feat: add brasil section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,16 @@ import { HeroComponent } from '../../sections/hero/hero.component';
 import { IntroComponent } from '../../sections/intro/intro.component';
 import { FranceComponent } from '../../sections/france/france.component';
 import { JapanComponent } from '../../sections/japan/japan.component';
+import { BrasilComponent } from '../../sections/brasil/brasil.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,IntroComponent,FranceComponent,JapanComponent],
+  imports: [HeroComponent,
+            IntroComponent,
+            FranceComponent,
+            JapanComponent,
+            BrasilComponent
+          ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/sections/brasil/brasil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brasil',
+  imports: [],
+  templateUrl: './brasil.component.html',
+  styleUrls: ['./brasil.component.scss','./brasil.responsive.component.scss']
+})
+export class BrasilComponent {
+
+}

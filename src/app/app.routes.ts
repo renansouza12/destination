f53b74de1edc96bb7f0f5943cee05ac4d544495d
renan <renansouza0122@gmail.com>
2025-03-14 01:33:35 -0400

    add france componente page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FranceComponent } from './sections/france/france.component';
+import { FrancePageComponent } from './pages/france-page/france-page.component';
 
 export const routes: Routes = [
     {   
         path:"",
         component:HomeComponent
+    },
+    {
+        path:"france",
+        component:FrancePageComponent
     }
 ];
---
+++ src/app/pages/france-page/france-page.component.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-france-page',
+  imports: [NgOptimizedImage],
+  templateUrl: './france-page.component.html',
+  styleUrl: './france-page.component.scss'
+})
+export class FrancePageComponent {
+
+}

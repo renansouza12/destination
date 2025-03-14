7a360045e167a2cc63e19e83e0dc171016180414
renan <renansouza0122@gmail.com>
2025-03-14 00:29:49 -0400

    feat(footer): add resposive size

--- src/app/sections/footer/footer.component.ts
+++ src/app/sections/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss','./footer.responsive.component.scss']
 })
 export class FooterComponent {
 

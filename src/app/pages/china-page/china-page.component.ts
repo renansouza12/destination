e9e651a6dd2d3bddfa91c33df1310dc2e4664207
RenanSouza <renansouza0122@gmail.com>
2025-03-22 23:52:41 -0300

    feat: add china images

--- src/app/pages/china-page/china-page.component.ts
+++ src/app/pages/china-page/china-page.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./china-page.component.scss','./china-page.responsive.component.scss']
 })
 export class ChinaPageComponent {
+  protected firstImage: string = "images/china-images/china3.jpg";
+  protected secondImage: string = "images/china-images/china2.avif";
+  protected thirdImage: string = "images/china-images/china1.jpg";
 
 }

a204c1184985e5e4dcd3c6cf89b00266eab37bc0
RenanSouza <renansouza0122@gmail.com>
2025-03-29 00:50:13 -0300

    feat(china-page): add angular animations to images

--- src/app/pages/china-page/china-page.component.ts
+++ src/app/pages/china-page/china-page.component.ts
@@ -1,11 +1,20 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { BackButtonComponent } from '../../components/back-button/back-button.component';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-china-page',
   imports: [NgOptimizedImage,BackButtonComponent],
   templateUrl: './china-page.component.html',
-  styleUrls: ['./china-page.component.scss','./china-page.responsive.component.scss']
+  styleUrls: ['./china-page.component.scss','./china-page.responsive.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition('void => visible', [ 
+        style({ opacity: 0 ,transform:"translate({{translateX}},{{translateY}})"}), 
+        animate('{{timing}}ms ease-in', style({ opacity: 1,transform:"translate(0px, 0px)"}))
+      ],{params:{ timing: 500, translateX: '0px', translateY: '0px' }})
+    ])
+  ]
 })
 export class ChinaPageComponent {
   protected firstImage: string = "images/china-images/china3.jpg";

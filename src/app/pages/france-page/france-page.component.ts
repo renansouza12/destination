adac05f5d7ea0ce75e4615abaa0f29e708c2d6b7
RenanSouza <renansouza0122@gmail.com>
2025-03-31 00:00:37 -0300

    add animation to france page images

--- src/app/pages/france-page/france-page.component.ts
+++ src/app/pages/france-page/france-page.component.ts
@@ -1,12 +1,21 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { BackButtonComponent } from '../../components/back-button/back-button.component';
+import { animate, query, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-france-page',
   imports: [NgOptimizedImage,BackButtonComponent],
   templateUrl: './france-page.component.html',
-  styleUrls:[ './france-page.component.scss','./france-page.responsive.component.scss']
+  styleUrls:[ './france-page.component.scss','./france-page.responsive.component.scss'],
+  animations:[
+    trigger('appear',[
+      transition('void => visible',[
+        style({opacity:0}),
+        animate("500ms ease-in",style({opacity:1}))
+      ])
+    ]) 
+  ]
 })
 export class FrancePageComponent {
     protected franceVideo:string = "videos/france-video.mp4";
--- src/app/pages/japan-page/japan-page.component.ts
+++ src/app/pages/japan-page/japan-page.component.ts
@@ -1,13 +1,21 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { BackButtonComponent } from "../../components/back-button/back-button.component";
+import { animate, transition, trigger,style } from '@angular/animations';
 
 @Component({
   selector: 'app-japan-page',
   imports: [NgOptimizedImage, BackButtonComponent],
   templateUrl: './japan-page.component.html',
-  styleUrls:[ './japan-page.component.scss','./japan-page.responsive.component.scss']
+  styleUrls:[ './japan-page.component.scss','./japan-page.responsive.component.scss'],
+  animations:[
+   trigger('slideIn',[
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' ,opacity:0}),
+        animate('500ms {{delay}}ms ease-in', style({ transform: 'translateX(0)',opacity:1 }))
+      ],{params:{delay:0}})
+    ])
+  ]
 })
 export class JapanPageComponent {
   protected japanVideo:string = "videos/japan-video.mp4"; 

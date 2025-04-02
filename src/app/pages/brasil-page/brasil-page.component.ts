31885fcd37bdd85f4dbb085c8713f73bd2d2d8b5
renan <renansouza0122@gmail.com>
2025-04-02 01:18:39 -0400

    feat: add scroll animation

--- src/app/pages/brasil-page/brasil-page.component.ts
+++ src/app/pages/brasil-page/brasil-page.component.ts
@@ -1,6 +1,7 @@
 import { NgOptimizedImage } from '@angular/common';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { BackButtonComponent } from '../../components/back-button/back-button.component';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-brasil-page',
@@ -8,7 +9,11 @@ import { BackButtonComponent } from '../../components/back-button/back-button.co
   templateUrl: './brasil-page.component.html',
   styleUrls: ['./brasil-page.component.scss', './brasil-page.responsive.component.scss'],
   animations: [
-    
+   trigger("premier",[
+    transition(":enter",[
+      
+    ])
+   ])
   ]
 })
 export class BrasilPageComponent implements AfterViewInit {
@@ -18,12 +23,12 @@ export class BrasilPageComponent implements AfterViewInit {
   protected brasilImage1: string = "images/brasil-images/brasil1.jpeg";
   protected brasilImage2: string = "images/brasil-images/brasil2.jpeg";
 
-  @ViewChild("brasilVideo") brasilVideo!: HTMLVideoElement;
+  @ViewChild("brasilVideo") brasilVideo!: ElementRef<HTMLVideoElement>;
 
 
   ngAfterViewInit(): void {
     if (this.brasilVideo) {
-      this.brasilVideo.muted = true;
+      this.brasilVideo.nativeElement.muted = true;
     }
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HeroComponent } from '../../sections/hero/hero.component';
 import { IntroComponent } from '../../sections/intro/intro.component';
 import { FranceComponent } from '../../sections/france/france.component';
@@ -6,6 +6,11 @@ import { JapanComponent } from '../../sections/japan/japan.component';
 import { BrasilComponent } from '../../sections/brasil/brasil.component';
 import { ChinaComponent } from '../../sections/china/china.component';
 import { FooterComponent } from '../../sections/footer/footer.component';
+
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 @Component({
   selector: 'app-home',
@@ -21,6 +26,99 @@ import { FooterComponent } from '../../sections/footer/footer.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy{
 
+  ngOnInit() {
+    // Register ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+    
+    this.initAnimations();
+  }
+
+  initAnimations() {
+    // Your animations will go here
+
+    gsap.from(".france_image",{
+      scrollTrigger:{
+        trigger: ".france_text",
+        start: "-20% center",
+        end: "top bottom",
+        scrub: 1,
+      },
+      y: 60,
+    })
+
+    gsap.from(".france_text",{
+      scrollTrigger:{
+        trigger: ".france_text",
+        start: "top center",
+        end: "bottom bottom",
+        scrub: 1,
+      },
+      y: -60
+    })
+
+    gsap.to("#japan_title",{
+      scrollTrigger:{
+        trigger:".japan_image",
+        start: "top center",
+        end: "bottom bottom",
+        scrub: 1, 
+        // markers:true
+      },
+      y:70
+    })
+
+    gsap.from(".japan_image",{
+      scrollTrigger:{
+        trigger: ".japan_image",
+        start: "top center",
+        end: "top center",
+        scrub: 1,
+        // markers:true
+      },
+      y: 20
+    })
+
+    gsap.from(".brasil_image",{
+      scrollTrigger:{
+        trigger: ".brasil_text",
+        start: "top center",
+        end: "top center",
+        scrub: 1,
+        // markers:true
+      },
+      y: 40
+    })
+
+
+
+    gsap.from(".china_image",{
+      scrollTrigger:{
+        trigger: ".china_text",
+        start: "top center",
+        end: "top center",
+        scrub: 1,
+        // markers:true
+      },
+      y: -40
+    })
+
+    gsap.to("#china_title",{
+      scrollTrigger:{
+        trigger:".china_image",
+        start: "top center",
+        end: "bottom bottom",
+        scrub: 1, 
+        // markers:true
+      },
+      y:70
+    })
+
+  }
+
+  ngOnDestroy() {
+    // Clean up ScrollTrigger instances
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+  }
 }

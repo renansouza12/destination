ad41edf32f86d5e760fb518c811e3c90f4e56958
renan <renansouza0122@gmail.com>
2025-04-10 01:22:50 -0400

    feat(slide) : add functionality to go to the next slide

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -2,7 +2,7 @@ import { animate, group, query, style, transition, trigger } from '@angular/anim
 import { NgOptimizedImage } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
-const timing = '500ms ease-in-out';
+const timing = '1000ms ease-in-out';
 
 @Component({
   selector: 'app-slide',
@@ -11,28 +11,16 @@ const timing = '500ms ease-in-out';
   styleUrl: './slide.component.scss',
   animations:[
     trigger('slideAnimation', [
-      transition(':increment', [
+      transition('* <=> *', [
         group([
           query(':enter', [
-            style({ transform: 'translateX(100%)', position: 'absolute', width: '100%' }),
-            animate(timing, style({ transform: 'translateX(0%)' }))
-          ], { optional: true }),
+            style({ transform: 'translateY(-150%)', opacity: 0 }),
+            animate(timing, style({ transform: 'translateY(0)', opacity: 1 }))
+          ]),
           query(':leave', [
-            style({ position: 'absolute', width: '100%' }),
-            animate(timing, style({ transform: 'translateX(-100%)' }))
-          ], { optional: true })
-        ])
-      ]),
-      transition(':decrement', [
-        group([
-          query(':enter', [
-            style({ transform: 'translateX(-100%)', position: 'absolute', width: '100%' }),
-            animate(timing, style({ transform: 'translateX(0%)' }))
-          ], { optional: true }),
-          query(':leave', [
-            style({ position: 'absolute', width: '100%' }),
-            animate(timing, style({ transform: 'translateX(100%)' }))
-          ], { optional: true })
+            style({ transform: 'translateY(0)', opacity: 1 }),
+            animate(timing, style({ transform: 'translateY(150%)', opacity: 0 }))
+          ])
         ])
       ])
     ])
@@ -40,34 +28,25 @@ const timing = '500ms ease-in-out';
 })
 export class SlideComponent {
   protected slide = signal(1);
-  protected animationParams = signal({
-    value: 0, // Unique value for each animation
-    params: {
-      leaveEnd: '100%',
-      enterStart: '-100%'
-    }
-  });
-  private totalSlides = 6;
-
-  nextSlide() {
-    const nextSlide = this.slide.update(value => (value < this.totalSlides ? value + 1 : 1));
-    this.animationParams.update(params => ({
-      value: params.value + 1, // Increment the unique value
-      params: {
-        leaveEnd: '-100%',
-        enterStart: '100%'
-      }
-    }));
+  protected totalSlides =6;
+  protected direction = signal<'right' | 'left'>('right');
+nextSlide() {
+  if (this.slide() < this.totalSlides) { 
+    this.direction.set("right");
+    this.slide.set(this.slide() + 1);
+  } else {
+    this.direction.set("right");
+    this.slide.set(1); // Loop back to the first slide
   }
+}
 
-  previousSlide() {
-    const prevSlide = this.slide.update(value => (value > 1 ? value - 1 : this.totalSlides));
-    this.animationParams.update(params => ({
-      value: params.value + 1, // Increment the unique value
-      params: {
-        leaveEnd: '100%',
-        enterStart: '-100%'
-      }
-    }));
+previousSlide() {
+  if (this.slide() > 1) {
+    this.direction.set("left");
+    this.slide.set(this.slide() - 1);
+  } else {
+    this.direction.set("left");
+    this.slide.set(this.totalSlides); // Loop back to the last slide
   }
+}
 }
